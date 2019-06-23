c4efa3f7b967c9a42c2a6d631831ea41736c236a
ocavue <ocavue@gmail.com>
2019-06-23 19:01:17 +0800

    Found a way to avoid this error!

--- pkg/main.ts
+++ pkg/main.ts
@@ -1,4 +1,6 @@
 // Defines a parser and serializer for [CommonMark](http://commonmark.org/) text.
 
-export { Token } from './token'
+import { Token } from './token'
+export type Token = Token
+// ref: https://github.com/facebook/create-react-app/issues/6054#issuecomment-481441272
 export { func } from "./func"
